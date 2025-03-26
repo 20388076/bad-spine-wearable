dbd9d167127d10709cdaa64a515546bcf54f7625
20388076 <bme20388076@uniwa.gr>
2025-03-26 17:10:16 +0200

    add senario 2 randomness, add senario 3 setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-int position = 105;             // Start at 105 degrees
+int position = 85;              // Start at 85 degrees
 int randomMoves = 10;           // Number of random movements
 int senario = 2;                // Senario of expirament
-static const int servoPin = 26; // Connect servo to pin 26
+static const int servoPin = 25; // Connect servo to pin 25 or D2
+unsigned long previousMillis = 0;
+int new_position = 0;         // New position for servo motor
+int last_position = position; // Last position of servo motor
+int step = 1;                 // Factor to decrease position by 1 degree
 
 Adafruit_MPU6050 mpu;
 Servo myServo;
@@ -19,7 +23,7 @@ TaskFunction_t Task1code2;
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
-//Task1code: blinks an LED every 1000 ms
+//Task1code: getting data from MPU6050
 void
 Task1code(void* pvParameters) {
     for (;;) {
@@ -31,54 +35,40 @@ Task1code(void* pvParameters) {
     }
 }
 
-//Task2code: blinks an LED every 700 ms
+//Task2code: move servo motor randomly in a range of 0-20 degrees
 void
 Task2code(void* pvParameters) {
-    static unsigned long previousMillis = 0;
-    int unsigned long interval = random(5000, 120000); // Move random every 5 seconds - 2 min
+    unsigned long interval = random(5000, 120000); // Move random every 5 seconds - 2 minutes
 
     for (;;) {
         unsigned long currentMillis = millis();
-
-        if (senario == 1) {
-            // Do Nothing
+        if (senario == 1) { // No movement detection
+            // myServo.write(40);
         } else if (senario == 2) {
             if (currentMillis - previousMillis >= interval) {
                 previousMillis = currentMillis; // Reset timer only after execution
-
-                int doNothing = random(0, 2); //` for randomness
-
-                if (doNothing == 1) {
-                    int newAngle = random(0, 21); // Random angle between 0 and 20
-                    //Serial.printf("\nWriting angle: %d to servo", newAngle);
-
-                    int tempPosition = position + newAngle;
-                    //Serial.printf("\nWriting angle: %d to servo and waiting 2 s...", tempPosition);
-
-                    myServo.write(tempPosition);     // Move to random position
-                    vTaskDelay(pdMS_TO_TICKS(2000)); // **Use vTaskDelay instead of delay()**
-
-                    myServo.write(position); // Return to the last known position*/
-                    //Serial.printf("\nAnd writing angle: %d back", position);
-                }
+                int newAngle = random(0, 21);   // Random angle between 0 and 20
+                int tempPosition = position + newAngle;
+                myServo.write(tempPosition);      // Move to random position
+                int ticks = random(900, 4000);    // Wait random from 900 ms -2 seconds
+                vTaskDelay(pdMS_TO_TICKS(ticks)); // **Use vTaskDelay instead of delay()**
+                myServo.write(position);          // Return to the last known position
             }
-            vTaskDelay(pdMS_TO_TICKS(10)); // Small delay to avoid watchdog reset
         }
 
-        // **Prevent Watchdog Timeout**
-
-        else if (senario == 3) {
-
-            // Now perform the gradual movement from 105° to 45° (1° per minute)
-            if (position > 45) {
-                position--;                       // Decrease position by 1 degree
-                myServo.write(position);          // Move the servo
-                vTaskDelay(pdMS_TO_TICKS(60000)); // Wait 1 minute (60,000 milliseconds)
+        else if (senario == 3) { // Moning gradually from 105° to 45° (1° per minute)
+            if (last_position > 45) {
+                new_position = last_position - step; // Decrease position by step in degrees
+                myServo.write(new_position);         // Move the servo
+                last_position = new_position;        // Update last position
+                int ticks = 60000;                   // Wait 1 minute (60,000 milliseconds)
+                vTaskDelay(pdMS_TO_TICKS(ticks));    // Wait 1 minute (60,000 milliseconds)
             }
         }
     }
 }
 
+// Try to initialize servo and mpu6050!
 void
 setup() {
     Serial.begin(115200);
@@ -87,9 +77,8 @@ setup() {
     randomSeed(analogRead(A0)); // Seed signals from port for random numbers
     Serial.begin(115200);
 
-    Serial.println("Write info for your project here e.g. version, author, etc.");
+    Serial.println("Wearable Posture Detection System, version:, author: ACHILLIOS PITTSILKAS");
 
-    // Try to initialize!
     if (!mpu.begin()) {
         Serial.println("Failed to find MPU6050 chip");
         while (1) {
@@ -98,7 +87,7 @@ setup() {
     }
     Serial.println("MPU6050 Found!");
 
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G); /* ToDo what are these? */
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G); // Setting accelerometer range of measurements to 8 G force
     Serial.print("Accelerometer range set to: ");
     switch (mpu.getAccelerometerRange()) {
         case MPU6050_RANGE_2_G: Serial.println("+-2G"); break;
@@ -107,7 +96,7 @@ setup() {
         case MPU6050_RANGE_16_G: Serial.println("+-16G"); break;
     }
 
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG); /* ToDo what are these? */
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG); // Setting gyro range of measurements to 500 degrees per second
     Serial.print("Gyro range set to: ");
     switch (mpu.getGyroRange()) {
         case MPU6050_RANGE_250_DEG: Serial.println("+- 250 deg/s"); break;
@@ -116,7 +105,7 @@ setup() {
         case MPU6050_RANGE_2000_DEG: Serial.println("+- 2000 deg/s"); break;
     }
 
-    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ); /* ToDo what are these? */
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ); // Setting mpu filter bandwidth to 5Hz
     Serial.print("Filter bandwidth set to: ");
     switch (mpu.getFilterBandwidth()) {
         case MPU6050_BAND_260_HZ: Serial.println("260 Hz"); break;
@@ -131,7 +120,7 @@ setup() {
     Serial.println("");
     delay(100);
 
-    //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+    // Create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
     xTaskCreatePinnedToCore(Task1code, /* Task function. */
                             "Task1",   /* name of task. */
                             10000,     /* Stack size of task */
@@ -141,7 +130,7 @@ setup() {
                             0);        /* pin task to core 0 */
     delay(500);
 
-    //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+    // Create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
     xTaskCreatePinnedToCore(Task2code, /* Task function. */
                             "Task2",   /* name of task. */
                             10000,     /* Stack size of task */
